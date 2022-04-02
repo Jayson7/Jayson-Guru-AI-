01a63931beff16ab3400775270e43e943377a679
jayson7 <lexxiijoo70@gmail.com>
2022-04-02 09:39:37 +0100

    jayson AI

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from http import client
 import wolframalpha # to answer queries involving math and weather etc.
 import wikipedia # to answer proled queries
 import re #to check for words
-import PySimpleGUI as sg # for our graphical user interface
+from tkinter import * # for our graphical user interface
 import datetime # to get the datetime
 import time 
 # save  app id for wolframalpha
-app_id = "YOUR_APP_ID"
+app_id = "G4UAUE-K5JX8XPVYX"
 client = wolframalpha.Client(app_id)
+
+# initialize Tk
+root = Tk()
+
+while True:  
+#     pass
 
 ''' input box with PySimpleGUI  which can take text input from the user''' 
 
@@ -23,25 +29,14 @@ def GreetUsersBasedOnTimeOfTheDay():
 
 GreetUsersBasedOnTimeOfTheDay()
 
-# while True:  
-#     pass
 
 
 
 
 # this section is for words manipulation and extraction 
-def inputBoxThatAcceptsTextAndNumbers():
-    layout = [[sg.Text('Enter your query')],
-              [sg.InputText()],
-              [sg.Button('Ok'), sg.Button('Cancel')]]
-    window = sg.Window('Input Box', layout)
-    event, values = window.read()
-
     # REG check for the words
     # re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
-    
-    window.close()
-    return values[0]
+
 
 
 
@@ -52,4 +47,5 @@ def inputBoxThatAcceptsTextAndNumbers():
 #         print(sub.plaintext)
 
 
-        
+
+root.mainloop()        
