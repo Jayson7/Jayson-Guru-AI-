978dca61079799234a7903c0898f94fbd40c1fbe
jayson7 <lexxiijoo70@gmail.com>
2022-04-02 03:37:47 +0100

    jayson AI

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ import wikipedia
 app_id = "G4UAUE-K5JX8XPVYX"
 client = wolframalpha.Client(app_id)
 
-res = client.query("who is the president of the United States?")
-print(next(res.results).text)
+# res = client.query("who is the president of the United States?")
+# res = client.query("what is the result of 2000 * 3420199")
+res = client.query("who is the muhammad buhari?")
+
+for pod in res.pods:
+    for sub in pod.subpods:
+        print(sub.plaintext)
+
+
+        
