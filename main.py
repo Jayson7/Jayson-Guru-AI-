529e5b12f3c90f19f1a3d2ace940877b6fa1fe71
jayson7 <lexxiijoo70@gmail.com>
2022-04-02 18:59:33 +0100

    jayson AI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from http import client
 from tkinter import font
-from turtle import color
+from turtle import color, width
 from numpy import delete, size
 import wolframalpha # to answer queries involving math and weather etc.
 import wikipedia # to answer proled queries
@@ -33,27 +33,27 @@ def GreetUsersBasedOnTimeOfTheDay():
         root.configure(bg='black')
     else:
         greet =  "Good Evening I'm Jayson AI, How can I help you ?"
-        greeter_lable = Label(root, text=greet, font =("Ubuntu", 15,) , fg='dark', anchor='center', pady =20, padx = 20, )
+        greeter_lable = Label(root, text=greet, font =("Ubuntu", 15,) , fg='black', anchor='center', pady =20, padx = 20, )
         root.configure(bg='white')
         
 GreetUsersBasedOnTimeOfTheDay()
-greeter_lable.grid(row=0, column=3, columnspan=3, sticky=W)
+greeter_lable.grid(row=1, column=3, columnspan=3)
 
 # dictacte screen size 
-root.geometry("540x540")
-root.minsize(540, 540)
-root.maxsize(540, 540)
+root.geometry("500x500")
+root.minsize(500, 500)
+root.maxsize(500, 500)
 root.title('Jayson Guru AI')
 # logo widget
 
 my_img = ImageTk.PhotoImage(Image.open('./4.jpg').resize((200, 150), Image.ANTIALIAS))
 my_label = Label(image=my_img)
 """ make the image fit the label """             
-my_label.grid(row=1, column=3, columnspan=3)
+my_label.grid(row=5, column=3, columnspan=3, pady=20)
       
 # input widget
 entry = Entry(root, width=30, borderwidth=4, font=('monospace', 17) )
-entry.grid(row=2, column=3, columnspan=3, padx=10, pady=60, ipady=6)
+entry.grid(row=6, column=3, columnspan=3, padx=10, pady=40, ipady=6)
 # entry placeholder text
 entry.insert(0, "Ask me anything") 
 def clear_placeholder(e):
@@ -74,35 +74,24 @@ def clear_placeholder():
     entry.config(fg='black')
 
 """ create a button to get the input from the user """
-button_submit = Button(root, text="Ask", font=('monospace', 12), command=get_input, bg='lightgreen', fg='black', padx=10, pady=10)
+button_submit = Button(root, text="Ask", font=('monospace', 12), command=get_input, bg='lightgreen', fg='black', padx=7, pady=7)
 """ place the button on the screen """
-button_submit.grid(row=3, column=2, columnspan=3, ipady=10, ipadx=10)
+button_submit.grid(row=8, column=2, columnspan=3, ipady=7, ipadx=7)
 """ create a button to clear the input """
-button_clear = Button(root, text="Clear", font=('monospace', 12), command=clear_placeholder, bg='lightgreen', fg='black', padx=10, pady=10)
+button_clear = Button(root, text="Clear", font=('monospace', 12), command=clear_placeholder, bg='lightgreen', fg='black', padx=7, pady=7)
 """ place the button on the screen """
-button_clear.grid(row=3, column=4, columnspan=3, ipady=10, ipadx=10)
+button_clear.grid(row=8, column=4, columnspan=3, ipady=7, ipadx=7)
+""" button_clear rounded corners """
+button_clear.config(relief=RIDGE)
+""" button_submit rounded corners """
+button_submit.config(relief=RIDGE)
 
-# while True:  
-    #pass
-''' input box with PySimpleGUI  which can take text input from the user''' 
-
-
-    
-
-
-
-    # this section is for words manipulation and extraction 
-        # REG check for the words
-        # re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
-
-
-
-
-    # res = client.query("who is the muhammad buhari?")
-
-    # for pod in res.pods:
-    #     for sub in pod.subpods:
-    #         print(sub.plaintext)
+""" search for input_text in wolframalpha """  
+res = client.query(input_text)
+for pod in res.pods:
+    for sub in pod.subpods:
+        print(sub.plaintext)
+        
 
 
 
