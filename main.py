0bdee2282a761358ce1d20989a17977c0da7f0cd
jayson7 <lexxiijoo70@gmail.com>
2022-04-02 04:20:26 +0100

    jayson Ai

--- main.py
+++ main.py
@@ -8,43 +8,48 @@ import time
 # save  app id for wolframalpha
 app_id = "G4UAUE-K5JX8XPVYX"
 client = wolframalpha.Client(app_id)
+
 ''' input box with PySimpleGUI  which can take text input from the user''' 
 
+# create a greeter function
+def GreetUsersBasedOnTimeOfTheDay():
+    hour = int(time.strftime("%H"))
+    if hour >= 0 and hour < 12:
+        print ("Good Morning")
+    elif hour >= 12 and hour < 18:
+        print ("Good Afternoon")
+    else:
+        print ("Good Evening")
+
+GreetUsersBasedOnTimeOfTheDay()
+
+# while True:  
+#     pass
+
+
+
+
+# this section is for words manipulation and extraction 
 def inputBoxThatAcceptsTextAndNumbers():
     layout = [[sg.Text('Enter your query')],
               [sg.InputText()],
               [sg.Button('Ok'), sg.Button('Cancel')]]
     window = sg.Window('Input Box', layout)
     event, values = window.read()
+
+    # REG check for the words
+    # re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+    
     window.close()
     return values[0]
-def GreetUsersBasedOnTimeOfTheDay():
-    hour = int(time.strftime("%H"))
-    if hour >= 0 and hour < 12:
-        return "Good Morning"
-    elif hour >= 12 and hour < 18:
-        return "Good Afternoon"
-    else:
-        return "Good Evening"
-    
-GreetUsersBasedOnTimeOfTheDay()
-while True:  
-    pass
 
 
 
+# res = client.query("who is the muhammad buhari?")
 
-
-# REG check for the words
-re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
-
-
-
-res = client.query("who is the muhammad buhari?")
-
-for pod in res.pods:
-    for sub in pod.subpods:
-        print(sub.plaintext)
+# for pod in res.pods:
+#     for sub in pod.subpods:
+#         print(sub.plaintext)
 
 
         
