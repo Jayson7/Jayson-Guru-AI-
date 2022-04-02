144cf7bf002c924b8d119850e61c9109413f9b72
jayson7 <lexxiijoo70@gmail.com>
2022-04-02 16:59:49 +0100

    jayson AI

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from http import client
 from tkinter import font
 from turtle import color
-from numpy import size
+from numpy import delete, size
 import wolframalpha # to answer queries involving math and weather etc.
 import wikipedia # to answer proled queries
 import re #to check for words
 from tkinter import * # for our graphical user interface
 import datetime # to get the datetime
 import time 
+from PIL import ImageTk, Image
+
 # save  app id for wolframalpha
 app_id = "G4UAUE-K5JX8XPVYX"
 client = wolframalpha.Client(app_id)
@@ -35,16 +37,27 @@ def GreetUsersBasedOnTimeOfTheDay():
         root.configure(bg='white')
         
 GreetUsersBasedOnTimeOfTheDay()
-greeter_lable.grid(row=0, column=0, columnspan=3)
+greeter_lable.grid(row=0, column=3, columnspan=3)
 
 # dictacte screen size 
 root.geometry("600x600")
 root.minsize(550, 550)
 root.maxsize(550, 550)
 root.title('Jayson Guru AI')
+# logo widget
 
-entry = Entry(root, width=50, borderwidth=5 )
-entry.grid(row=2, column=0, columnspan=3, padx=20, pady=100, ippady=10)
+my_img = ImageTk.PhotoImage(Image.open('./4.jpg').resize((150, 150), Image.ANTIALIAS))
+my_label = Label(image=my_img)
+""" make the image fit the label """             
+my_label.grid(row=1, column=0, columnspan=3)
+      
+# input widget
+entry = Entry(root, width=30, borderwidth=4, font=('monospace', 17) )
+entry.grid(row=2, column=0, columnspan=3, padx=10, pady=60, ipady=6)
+# entry placeholder text     #  
+
+
+
 
 # while True:  
     #pass
